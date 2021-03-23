da0a2ca3bfc1abec8a74e0b06965507b25386203
Kevin Gliewe <kevingliewe@gmail.com>
2021-03-23 17:19:15 +0100

    Add unversal string line splitting

---
+++ src/GCore.Source/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GCore.Source.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Splits the string into lines based on '\r\n', '\r' or '\n'
+        /// </summary>
+        /// <param name="this">String with lines</param>
+        /// <returns>Individual lines</returns>
+        public static string[] SplitNewLine(this string @this)
+        {
+            return @this.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+        }
+    }
+}
